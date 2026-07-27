fc2d249e55ed3b941fdff840f7e4ab445efdc50e
Deepak Sahu <ds2998625@gmail.com>
2026-07-27 16:40:33 +0530

    feat: implement automatic data seeding system for 200+ jobs

--- backend/jobs/apps.py
+++ backend/jobs/apps.py
@@ -1,5 +1,9 @@
 from django.apps import AppConfig
-
+import threading
 
 class JobsConfig(AppConfig):
     name = 'jobs'
+
+    def ready(self):
+        from jobs.seeder import seed_db_if_empty
+        threading.Thread(target=seed_db_if_empty, daemon=True).start()
